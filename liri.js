d95be53f5e6d2b785c460d3e76b5813b32a5d06f
Koxinga <timwu1015@gmail.com>
2018-12-08 21:01:58 -0500

    functional game

--- liri.js
+++ liri.js
@@ -3,8 +3,41 @@ require("dotenv").config();
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
 var moment = require("moment");
+var keys = require("./keys");
 
-// var spotify = new Spotify(keys.spotify);
+function spotify(song) {
+    var spotify = new Spotify(keys.spotify);
+    
+    spotify.search({type: "track", query: song}, function(err, data) {
+        if(err) {
+            return console.log("Error occured: " + err);
+        }
+    var parse = data.tracks.items;
+    console.log("\nArtist(s): " + parse[0].artists);
+    console.log("Song's Name: " + parse[0].name);
+    console.log("Preview: " + parse[0].preview_url);
+    console.log("Album: " + parse[0].album.name + "\n")
+    });
+}
+
+function imdb(movie) {
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    // console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function(response) {
+          var data = response.data;
+          console.log("\nTitle of the movie: " + data.Title);
+          console.log("Year released: " + data.Year);
+          console.log("IMDB rating: " + data.imdbRating);
+          console.log("Rotton Tomatoes rating: " + data.Ratings[2].Value);
+          console.log("Country produced: " + data.Country);
+          console.log("Languages: " + data.Language);
+          console.log("Plot: " + data.Plot);
+          console.log("Actors: " + data.Actors + "\n");
+        }
+      );
+}
 
 if (process.argv[2] === "concert-this") {
     var artist = process.argv[3];
@@ -25,4 +58,26 @@ if (process.argv[2] === "concert-this") {
         }
     );
 }
+else if (process.argv[2] === "spotify-this-song") {
+    if (!process.argv[3]) {
+        var artist = "The Sign";
+        spotify(artist);
+    }
+    else {
+        var artist = process.argv[3];
+        spotify(artist);
+    }
+}
+else if(process.argv[2] === "movie-this") {
+    if (!process.argv[3]) {
+        var movie = "Mr.Nobody";
+        imdb(movie);
+    }
+    else {
+        var movie = process.argv[3];
+        movie = movie.replace(/\s/g, "+");
+        imdb(movie);
+    }
+}
+
 
